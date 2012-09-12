154442376991aa8ba7d98b9bbec8e983676c2b95
TJ Holowaychuk <tj@vision-media.ca>
2012-09-12 13:10:39 -0700

    add some more test setup

--- test/server.js
+++ test/server.js
@@ -10,9 +10,15 @@ var app = express();
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 
+app.get('/', function(req, res){
+  res.sendfile('test/tests.html');
+});
+
 app.post('/upload', function(req, res){
   console.log(req.files);
 });
+
+app.use(express.static(__dirname));
 
 app.listen(3000);
 console.log('listening on port 3000');
